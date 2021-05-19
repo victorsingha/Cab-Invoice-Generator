53aaa982d1e99de93aa44c4ff917345abb9af6e9
victor-singha <vicunite2@gmail.com>
2021-05-19 13:10:15 +0530

    initial commit

---
+++ CabInvoiceGenerator/Ride.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CabInvoiceGenerator
+{
+    public class Ride
+    {
+        public double distance;
+        public int time;
+        public Ride(double distance, int time)
+        {
+            this.distance = distance;
+            this.time = time;
+        }
+    }
+}
---
+++ CabInvoiceGeneratorTest/UnitTest1.cs
@@ -0,0 +1,36 @@
+using CabInvoiceGenerator;
+using NUnit.Framework;
+
+namespace CabInvoiceGeneratorTest
+{
+    public class Tests
+    {
+        InvoiceGenerator invoiceGenerator = null;
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void GivenDistanceAndTimeShouldReturnTotalFare()
+        {
+            invoiceGenerator = new InvoiceGenerator(RideType.NORMAL);
+            double distance = 2.0;
+            int time = 5;
+            double fare = invoiceGenerator.CalculateFare(distance, time);
+            double expected = 25;
+            Assert.AreEqual(expected, fare);
+        }
+
+        [Test]
+        public void GivenMultipleRideShouldReturnInvoiceSummary()
+        {
+            invoiceGenerator = new InvoiceGenerator(RideType.NORMAL);
+            Ride[] rides = { new Ride(2.0, 5), new Ride(0.1, 5) };
+            InvoiceSummary summary = invoiceGenerator.CalculateFare(rides);
+            InvoiceSummary expectedSummary = new InvoiceSummary(2, 35.0);
+            Assert.AreEqual(expectedSummary.GetType(), summary.GetType());
+        }
+
+    }
+}
